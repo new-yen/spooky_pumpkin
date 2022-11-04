446f4158c4a43831cba134be2ba66ae30fa56d97
new-yen <33000475+new-yen@users.noreply.github.com>
2022-11-04 19:13:18 +0100

    Update spooky_script.py

--- code/spooky_script.py
+++ code/spooky_script.py
@@ -6,6 +6,7 @@ from time import sleep
 from bluetool import Bluetooth
 
 b = Bluetooth()
+
 pir = MotionSensor(4)
 led = LED(17)
 
@@ -15,8 +16,13 @@ def rndmp3():
     call(["aplay", file])
 
 while True:
-    if b.get_connected_devices():
+    if b.get_connected_devices() == []:
+        print("no device connected, scanning for 10sec")
+        b.scan(10)
+        b.connect("C9:50:76:E4:38:AC")      #MAC address of my bluetooth speaker;paired and trusted beforehand via terminal & "bluetoothctl"
         print("connected")
+    
+    else:
         pir.wait_for_motion()
         print("motion detected")
         led.blink(on_time=0.2, off_time=0.2, n=10, background=True)
@@ -24,9 +30,4 @@ while True:
         led.blink(on_time=1, off_time=1, n=5, background=False)
         led.blink(on_time=5, off_time=0, n=1, background=False)
         sleep(5)
-    else:
-        print("not connected, looking for bluetooth speaker")
-        b.connect("C9:50:76:E4:38:AC")      #MAC address of my bluetooth speaker;paired and trusted beforehand via terminal & "bluetoothctl"
-
-
 
